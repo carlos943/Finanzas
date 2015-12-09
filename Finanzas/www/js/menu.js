3ccf1c4891c109878b7576b9124c266bf4dc0769
a09000847 <a09000847@fmat-EXCC4-A1.inet.uady.mx>
2015-12-09 12:05:52 -0600

    menu en blance solo se puede ver con los botones

--- Finanzas/www/js/menu.js
+++ Finanzas/www/js/menu.js
@@ -42,16 +42,5 @@
         }
     });
     
-     $( document ).on( "swipeleft swiperight", "#balance", function( e ) {
-        // We check if there is no open panel on the page because otherwise
-        // a swipe to close the left panel would also open the right panel (and v.v.).
-        // We do this by checking the data that the framework stores on the page element (panel: open).
-        if ( $( ".ui-page-active" ).jqmData( "panel" ) !== "open" ) {
-            if ( e.type === "swipeleft" ) {
-                $( "#panelIzquierdo" ).panel( "close" );
-            } else if ( e.type === "swiperight" ) {
-                $( "#panelIzquierdo" ).panel( "open" );
-            }
-        }
-    });
+
 
